f1aeb65a12842e21daec5120dc3bcca72babe75f
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-12-30 23:16:33 +0000

    fix: Update status URL and variable name
    Update status URL from API to GitHub and fix variable name

--- update-status.py
+++ update-status.py
@@ -1,15 +1,27 @@
 import os
-import requests
+import sys
+
+# Install requests module if not already installed
+try:
+    import requests
+except ImportError:
+    print("Installing requests module...")
+    try:
+        os.system(f"{sys.executable} -m pip install requests")
+        import requests  # Retry import after installation
+    except Exception as e:
+        print(f"Failed to install and import requests module: {e}")
+        sys.exit(1)
 
 token = os.environ.get('$GITHUB_TOKEN')
 username = os.environ.get('$GITHUB_USERNAME')
 
 def main():
     try:
-        user_response = requests.get(f'https://api.github.com/{username}/{username}', headers={'Authorization': f'Bearer {token}'})
+        user_response = requests.get(f'https://github.com/{username}/{username}', headers={'Authorization': f'Bearer {token}'})
         user_data = user_response.json()
 
-        total_repos = user_data['public_repos']
+        total_repos = user_data['$public_repos']
 
         # Add more API calls and processing for other information
 
@@ -17,7 +29,7 @@ def main():
         # Update GitHub status using appropriate API calls
     except Exception as e:
         print(f'Error updating GitHub status: {e}')
-        exit(1)
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
